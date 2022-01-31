7374abf775f4e97fe5ff30b4c307878ac73977d5
TeddyH08 <teddy.hemonet@gmail.com>
2022-01-31 09:47:09 +0100

    first design

---
+++ assets/php/navbar.php
@@ -0,0 +1,12 @@
+<header class="header__navbar">
+    <div class="navbar">
+        <ul class="list_navl">
+            <li><a class="acceuil" href="index.php">Binouze</a></h2></li>
+            <li><a href="#" class="biere">Liste des bières</a></li>
+        </ul>
+        <ul class="list_navr">
+            <li style="float:right"><a href="#" class="co">Connexion</a></li>
+            <li style="float:right"><a href="#" class="ins">Inscription</a></li>
+        </ul>
+    </div>
+</header>
